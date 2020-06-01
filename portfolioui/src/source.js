a85cab24534d1afed4b43c6e065d0d9760881b1c
CORPINDIA\EI11376 <satvik.nallapoola@evry.com>
2020-06-01 19:32:32 +0530

    changes for image path

--- portfolioui/src/source.js
+++ portfolioui/src/source.js
@@ -1,9 +1,9 @@
 export default [
-    {id:1 , path:'../assets/thumb32.png' , name:'Inventive Design' , description:'Building a Customer-Centric Organization 1'},
-    {id:2 , path:'../assets/thumb32.png' , name:'Inventive' , description:'Building a Customer-Centric Organization 2'},
-    {id:3 , path:'../assets/thumb32.png' , name:'Design' , description:'Building a Customer-Centric Organization 3'},
-    {id:4 , path:'../assets/thumb32.png' , name:'Innovational Design' , description:'Building a Customer-Centric Organization 4'},
-    {id:5 , path:'../assets/thumb32.png' , name:'Inventive Design App' , description:'Building a Customer-Centric Organization 5'},
-    {id:6 , path:'../assets/thumb32.png' , name:'Inventive App' , description:'Building a Customer-Centric Organization 6'},
-    {id:7 , path:'../assets/thumb32.png' , name:'Inventive App Design' , description:'Building a Customer-Centric Organization 7'}
+    {id:1 , image: 'thumb32.png' , name:'Inventive Design' , description:'Building a Customer-Centric Organization 1'},
+    {id:2 , image: 'thumb32.png' , name:'Inventive' , description:'Building a Customer-Centric Organization 2'},
+    {id:3 , image: 'thumb32.png' , name:'Design' , description:'Building a Customer-Centric Organization 3'},
+    {id:4 , image: 'thumb32.png' , name:'Innovational Design' , description:'Building a Customer-Centric Organization 4'},
+    {id:5 , image: 'thumb32.png', name:'Inventive Design App' , description:'Building a Customer-Centric Organization 5'},
+    {id:6 , image: 'thumb32.png', name:'Inventive App' , description:'Building a Customer-Centric Organization 6'},
+    {id:7 , image: 'thumb32.png' , name:'Inventive App Design' , description:'Building a Customer-Centric Organization 7'}
 ]
