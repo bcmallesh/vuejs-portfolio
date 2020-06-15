5612c6b6f9699970d4835b4b6efc93550b69d4f4
CORPINDIA\EI11376 <satvik.nallapoola@evry.com>
2020-06-15 16:01:09 +0530

    Added static sections , section summary , tags components in description page

---
+++ portfolioui/src/components/SectionSummary.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+          <div class="col-md-6">
+                            <h5> Packed with the most important insurance agents
+                                reporting, analytics, and tracking features</h5>
+                        </div>
+                        <div class="col-md-6">
+                            <img class="imgq" src="assets/images/screens/pulse_app/1.png" alt="UI">
+                        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.imgq {
+  position: absolute;
+  z-index: 2;
+  width: 90%;
+  left: 7%;
+  top: -118%;
+}
+</style>
---
+++ portfolioui/src/components/TagsSection.vue
@@ -0,0 +1,48 @@
+<template>
+       <div class="boxsm">
+                            <div class="box1">
+                                <img src="assets/images/SVG/brush1.svg" alt="">UI/UX Design
+                            </div>
+                            <div class="box2">
+                                <img src="assets/images/SVG/code.svg" alt="">UI Development
+                            </div>
+        </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.boxsm {
+  display: -webkit-inline-box;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  margin-left: 16px;
+}
+
+.boxsm .box2 {
+  border: 1px solid #ece9e9;
+  padding: 10px;
+  padding-left: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: 11px;
+  border-radius: 30px;
+}
+
+.boxsm .box1 {
+  border: 1px solid #ece9e9;
+  padding: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-radius: 30px;
+}
+
+.boxsm img {
+  height: 25px;
+  width: 25px;
+  margin-right: 11px;
+}
+</style>
